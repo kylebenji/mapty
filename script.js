c609e6334ee8510f6506584edaa1a7e27e767e92
kylebenji <127806283+kylebenji@users.noreply.github.com>
2023-04-13 11:01:22 -0700

    adding button to workout panes

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ class Workout {
     return `<li class="workout workout--${this.type}" data-id="${this.id}">
           <h2 class="workout__title">${this.description}</h2>
           ${innerHTML}
+          <button class="workout__delete">x</button>
         </li>`;
   }
 }
